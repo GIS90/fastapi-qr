2be76806b5b17c9c91bece61d09b224db89389a7
mingliang.gao <gaoming971366@163.com>
2024-10-09 22:24:39 +0800

    新增：path-str-regex

--- deploy/view/base.py
+++ deploy/view/base.py
@@ -97,7 +97,28 @@ async def user(rtx_id: str):
 
 
 @base.get('/user/path-str/{rtx_id}',
-          summary="**Path限制**字符型资源参数请求示例",
+          summary="**Path限制**字符型资源参数请求示例[非正则路径]",
+          description="参数为字符型，使用fastapi.Path进行参数条件限制，包含description[描述]，min_length[最小长度]，max_length[最大长度]，regex[正则表达式]，参数限制可省略，"
+                      "如果使用Path定义参数为必填参数，第一个参数为...（看源码是语法糖写法）"
+          )
+async def user(
+        rtx_id: str = Path(..., description="[字符型]资源参数rtx_id", min_length=1, max_length=12)
+):
+    """
+    Path限制字符型资源参数请求示例
+    :param rtx_id: [str]资源请求参数
+    :return: json
+    """
+    return Status(
+        Status_code.CODE_100_SUCCESS,
+        Status_enum.SUCCESS,
+        Status_msg.get(100),
+        {'rtx_id': rtx_id}
+    ).status_body
+
+
+@base.get('/user/path-str-regex/{rtx_id}',
+          summary="**Path限制**字符型资源参数请求示例[正则路径]",
           description="参数为字符型，使用fastapi.Path进行参数条件限制，包含description[描述]，min_length[最小长度]，max_length[最大长度]，regex[正则表达式]，参数限制可省略，"
                       "如果使用Path定义参数为必填参数，第一个参数为...（看源码是语法糖写法）"
           )
