12f84605d855dc104ea417bd938ecad14218e115
mingliang.gao <gaoming971366@163.com>
2024-10-10 22:31:03 +0800

    优化代码

--- deploy/view/depend.py
+++ deploy/view/depend.py
@@ -3,11 +3,16 @@
 """
 ------------------------------------------------
 
-describe: 
+describe:
+    depend
+    依赖的好处：
+        1、提高代码的复用率
+        2、共享数据库连接
+        3、增强安全、认证、角色管理
 
 base_info:
     __author__ = "PyGo"
-    __time__ = "2024/8/21 21:45"
+    __time__ = "2024/10/10 22:21"
     __version__ = "v.1.0.0"
     __mail__ = "gaoming971366@163.com"
     __blog__ = "www.pygo2.top"
@@ -30,32 +35,24 @@ Life is short, I use python.
 """
 
 # ------------------------------------------------------------
-# usage: /usr/bin/python zlxcx.py
+# usage: /usr/bin/python depend.py
 # ------------------------------------------------------------
-from fastapi import APIRouter, Query, Request
+from fastapi import APIRouter, Request
 
 from deploy.utils.status import Status
 from deploy.utils.status_value import StatusEnum as Status_enum, \
     StatusMsg as Status_msg, StatusCode as Status_code
 
-from deploy.body.api import ApiZlxcxProcessModel
-from deploy.service.api import ApiService
-
 
 # define view
-api = APIRouter(prefix="/api", tags=["APIs集合"])
-api_service = ApiService()
+depend = APIRouter(prefix="/depend", tags=["Depend依赖注入"])
 
 
-@api.post('/zlxcx/process',
-          summary="[质量小程序APIs]过程检查",
-          description="项目过程检查的详情数据"
+@depend.get('/',
+          summary="Welcome to FastAPI-QR脚手架",
+          description="Hello FastAPI-QR脚手架!"
           )
-async def zlxcx_process(params: ApiZlxcxProcessModel):
+async def hi():
     """
-    质量小程序: 过程检查
-    :return: json data
-    :param params: [dict]查询请求参数
-    :return: json
+    :return: JSON
     """
-    return api_service.zlxcx_process(params.model_dump())
